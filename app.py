f01f6ad76e466219f92bffc2f54da4fd372e70ee
kalinharikumar <127252539+kalinharikumar@users.noreply.github.com>
2023-04-10 15:27:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ import streamlit as st
 import easyocr
 import cv2
 import re
+from sqlalchemy import text
+import sqlalchemy
+import pymysql
 
 st.title(':blue[_BizCardX_]')
 st.caption('Business Card scanner')
-tab1, tab2, tab3 = st.tabs(["Home", "Update" ,"About"])
+tab1, tab2 = st.tabs(["Home", "About"])
 
-def scan(path):
-  #img = cv2.imread(path)
-  reader = easyocr.Reader(['en'])
-  result = reader.readtext(path,detail = 0, paragraph = True)
-  return result
+#Establishing DataBase connection through sqlalchemy-engine
+#with cloud server using PlanetScale platform
+ssl_args = {'ssl_ca':'/etc/ssl/certs/ca-certificates.crt'}
+engine = sqlalchemy.create_engine("mysql+pymysql://5uxix5r7v1ftz5kblzxe:pscale_pw_TIUsd2vFjGgVveKpP1reNw9XUb5D0NraxGEpmvn0vaY@aws.connect.psdb.cloud/kalin",connect_args=ssl_args)
+connection=engine.connect()
+tables=engine.table_names()
 
 with tab1:
     # col1, col2=st.columns(2)
@@ -22,10 +26,10 @@ with tab1:
     if uploaded_file != None:
         st.image(uploaded_file)
         #main part
-        with open(f'{file_name}.png', 'wb') as f:
-            f.write(uploaded_file.getvalue())
-        
-        img = cv2.imread(f'{file_name}.png')
+        up=' '
+        with open(f'{up}.png', 'wb') as f:
+          f.write(uploaded_file.getvalue())
+        img = cv2.imread(f'{up}.png')
         reader = easyocr.Reader(['en'])
         l = reader.readtext(img,detail = 0, paragraph = True)
         s=' '.join(l)
@@ -66,7 +70,7 @@ with tab1:
         nam_l = re.findall(r"[A-Za-z]+\s[A-Za-z]+|[A-Za-z]+",nam_s)
         nam = nam_l[0]
         cmp = l_s[-1]
-        
+
         st.write('Extracted Raw Data:',s)
         st.subheader(':blue[_Check the Details & Save_]')
 
@@ -77,7 +81,7 @@ with tab1:
             mobile_number = st.text_input('Mobile Number:', value=mob)
             email_address = st.text_input('Email Address:', value=email)
             website = st.text_input('Website:', value=url)
-            area = st.text_input('Area:', value=area+v)
+            area = st.text_input('Area:', value=area_v)
             city = st.text_input('City:', value=city_v)
             state = st.text_input('State:', value=state_v)
             pincode = st.text_input('Pincode:', value=pin)
@@ -88,13 +92,33 @@ with tab1:
         if submit_button or st.session_state.load_state:
             st.session_state.load_state = True
             # databade upload
+            #Inserting into db
+            if tables == []:
+              connection.execute(text("CREATE TABLE card_details (id Integer(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,company VARCHAR(20),name VARCHAR(20),designation VARCHAR(20),mobile VARCHAR(30),email VARCHAR(20),website VARCHAR(20),area VARCHAR(20),city VARCHAR(20),state VARCHAR(20),pincode VARCHAR(10),photo MEDIUMBLOB)"))
+              query=text("INSERT INTO card_details (company,name,designation,mobile,email,website,area,city,state,pincode,photo) VALUES (:company,:name,:designation,:mobile,:email,:website,:area,:city,:state,:pincode,:photo)")
+              connection.execute(query,company=cmp,name=nam,designation=des,mobile=mob,email=email,website=url,area=area,city=city,state=state,pincode=pin,photo=img)
+              st.success('Saved to Database', icon="✅")
+
+            if tables != []:
+              query=text("INSERT INTO card_details (company,name,designation,mobile,email,website,area,city,state,pincode,photo) VALUES (:company,:name,:designation,:mobile,:email,:website,:area,:city,:state,:pincode,:photo)")
+              connection.execute(query,company=cmp,name=nam,designation=des,mobile=mob,email=email,website=url,area=area,city=city,state=state,pincode=pin,photo=img)
+              st.success('Saved to Database', icon="✅")
+      
+#with tab2:
+#st.subheader(':blue[_Update details_]')
+#search = st.text_input('Enter Card Holder Name')
+#if st.button('search'):
+#st.write('will be searched')
+
 
 with tab2:
-    st.subheader(':blue[_Update details_]')
-    search = st.text_input('Enter Card Holder Name')
-    if st.button('search'):
-        st.write('will be searched')
-
-
-with tab3:
-    st.subheader(':blue[_about_]')
+    st.subheader(':blue[_How to use:_]')
+    st.write('1. upload the image from local storage')
+    st.write('2. check the details and do corrections if needed')
+    st.write('3. finally upload to database by clicking save button')
+    st.subheader(':blue[_Packages used:_]')
+    st.write('easyocr, streamlit, cv2, regex, sqlalchemy, pymysql')
+    st.subheader(':blue[_Demo video:_]')
+    st.write('')
+    st.subheader(':blue[_GitHub link:_]')
+    st.write('https://github.com/kalinharikumar/BizCardX.git')
